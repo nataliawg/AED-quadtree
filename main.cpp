75e94b17a28bc4c24fbe6d2bfeaf78c1009aeb11
nataliawg <natalia.watson@utec.edu.pe>
2024-12-02 01:37:33 -0500

    avance jijij

--- main.cpp
+++ main.cpp
@@ -1,177 +1,37 @@
+
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <vector>
-#include <stdexcept>
+#include "quadtree.cpp"
 using namespace std;
 
-// Basado en https://www.geeksforgeeks.org/quad-tree/
-
-class Punto {
-public:
-    int x;
-    int y;
-
-    Punto(int xvalue, int yvalue) : x(xvalue), y(yvalue) {}
-};
-
-template<class T>
-class NodoQuad {
-public:
-    Punto punto;
-    T dato;
-    NodoQuad* NW, * NE, * SW, * SE;
-    NodoQuad(int x, int y, T data) : punto(Punto(x, y)), dato(data), NW(NULL), NE(NULL), SE(NULL), SW(NULL) {}
-};
-
-template<class T>
-class QuadTree {
-private:
-    NodoQuad<T>* root;
-    int limitX, limitY;
-
-public:
-    QuadTree(int x, int y) : limitX(x), limitY(y), root(NULL) {}
-
-    int compare(Punto R, Punto K) {
-        // Si retorna 1 es NW, si retorna 2 es NE, si es 3 SW, si es 4 es SE.
-
-        if (R.x > K.x && R.y > K.y) {
-            return 1;
-        }
-        else if (R.x < K.x && R.y > K.y) {
-            return 2;
-        }
-        else if (R.x > K.x && R.y < K.y) {
-            return 3;
-        }
-        else if (R.x < K.x && R.y < K.y) {
-            return 4;
-        }
-
-        //si es 0 esta fuera del rango
-        return 0;
-    }
-
-    void insertInTree(NodoQuad<T>* R, Punto K, T tag) {
-        int comp = compare(R->punto, K);
-
-        if (comp == 1) {
-            if (R->NW != NULL) {
-                insertInTree(R->NW, K, tag);
-            }
-            else {
-                R->NW = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-        else if (comp == 2) {
-            if (R->NE != NULL) {
-                insertInTree(R->NE, K, tag);
-            }
-            else {
-                R->NE = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-        else if (comp == 3) {
-            if (R->SW != NULL) {
-                insertInTree(R->SW, K, tag);
-            }
-            else {
-                R->SW = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-        else if (comp == 4) {
-            if (R->SE != NULL) {
-                insertInTree(R->SE, K, tag);
-            }
-            else {
-                R->SE = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-    }
-
-    void insert(Punto K, T tag) {
-        if (root == NULL) {
-            root = new NodoQuad<T>(K.x, K.y, tag);
-            cout << "Árbol estaba vacío, insertando root" << endl;
-            return;
-        }
-
-        insertInTree(root, K, tag);
-    }
-
-    NodoQuad<T>* deleteNode(NodoQuad<T>* R, Punto K) {
-        if (R == NULL) return NULL;
-
-        // Si encontramos el nodo a eliminar
-        if (R->punto.x == K.x && R->punto.y == K.y) {
-            delete R;
-            return NULL;
-        }
-
-        int comp = compare(R->punto, K);
-
-        if (comp == 1) {
-            R->NW = deleteNode(R->NW, K);
-        }
-        else if (comp == 2) {
-            R->NE = deleteNode(R->NE, K);
-        }
-        else if (comp == 3) {
-            R->SW = deleteNode(R->SW, K);
-        }
-        else if (comp == 4) {
-            R->SE = deleteNode(R->SE, K);
-        }
-
-        return R;
-    }
-
-    void remove(Punto K) {
-        if (root == NULL) {
-            cout << "El árbol está vacío, no se puede eliminar." << endl;
-            return;
-        }
-
-        root = deleteNode(root, K);
-    }
-
-    void getStruct() {
-        if (!root) {
-            cout << "Árbol vacío." << endl;
-            return;
-        }
-
-        cout << "Raíz: " << root->dato << endl;
-        if (root->NW) cout << "Raíz NW: " << root->NW->dato << endl;
-        if (root->NE) cout << "Raíz NE: " << root->NE->dato << endl;
-        if (root->SW) cout << "Raíz SW: " << root->SW->dato << endl;
-        if (root->SE) cout << "Raíz SE: " << root->SE->dato << endl;
-    }
-};
-
 int main() {
-    QuadTree<char> Quadtree(127, 127);
 
-    Punto A(70, 70);
-    Punto B(20, 20);
-    Punto C(100, 100);
-    Punto D(60, 80);
-    Punto E(5, 5);
-    Punto F(50, 50);
+    //crea la ventana
+    sf::RenderWindow window(sf::VideoMode(800, 600), "quadtree c:");
 
-    Quadtree.insert(A, 'A');
-    Quadtree.insert(B, 'B');
-    Quadtree.insert(C, 'C');
-    Quadtree.insert(D, 'D');
-    Quadtree.insert(E, 'E');
-    Quadtree.insert(F, 'F');
+    QuadTree<char> quadtree(400, 300);
 
-    cout << "Estructura inicial del árbol:" << endl;
-    Quadtree.getStruct();
+    quadtree.insert(Punto(100, 100), 'A');
+    quadtree.insert(Punto(200, 150), 'B');
+    quadtree.insert(Punto(300, 200), 'C');
+    quadtree.insert(Punto(500, 100), 'D');
+    quadtree.insert(Punto(600, 300), 'E');
 
-    // Eliminar un nodo
-    cout << "\nEliminando el nodo B (20, 20):" << endl;
-    Quadtree.remove(B);
-    Quadtree.getStruct();
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::VertexArray lines(sf::Lines);
+        quadtree.draw(window, quadtree.root, lines);
+        window.draw(lines);
+
+        window.display();
+    }
 
     return 0;
 }
---
+++ quadtree.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+// Basado en https://www.geeksforgeeks.org/quad-tree/
+
+class Punto {
+public:
+    int x;
+    int y;
+
+    Punto(int xvalue, int yvalue) : x(xvalue), y(yvalue) {}
+};
+
+template<class T>
+class NodoQuad {
+public:
+    Punto punto;
+    T dato;
+    NodoQuad* NW, * NE, * SW, * SE;
+    NodoQuad(int x, int y, T data) : punto(Punto(x, y)), dato(data), NW(NULL), NE(NULL), SE(NULL), SW(NULL) {}
+};
+
+template<class T>
+class QuadTree {
+
+public:
+
+    NodoQuad<T>* root;
+    int limitX, limitY;
+
+    QuadTree(int x, int y) : limitX(x), limitY(y), root(NULL) {}
+
+    int compare(Punto R, Punto K) {
+        // Si retorna 1 es NW, si retorna 2 es NE, si es 3 SW, si es 4 es SE.
+
+        if (R.x > K.x && R.y > K.y) {
+            return 1;
+        }
+        else if (R.x < K.x && R.y > K.y) {
+            return 2;
+        }
+        else if (R.x > K.x && R.y < K.y) {
+            return 3;
+        }
+        else if (R.x < K.x && R.y < K.y) {
+            return 4;
+        }
+
+        //si es 0 esta fuera del rango
+        return 0;
+    }
+
+    void insertInTree(NodoQuad<T>* R, Punto K, T tag) {
+        int comp = compare(R->punto, K);
+
+        if (comp == 1) {
+            if (R->NW != NULL) {
+                insertInTree(R->NW, K, tag);
+            }
+            else {
+                R->NW = new NodoQuad<T>(K.x, K.y, tag);
+            }
+        }
+        else if (comp == 2) {
+            if (R->NE != NULL) {
+                insertInTree(R->NE, K, tag);
+            }
+            else {
+                R->NE = new NodoQuad<T>(K.x, K.y, tag);
+            }
+        }
+        else if (comp == 3) {
+            if (R->SW != NULL) {
+                insertInTree(R->SW, K, tag);
+            }
+            else {
+                R->SW = new NodoQuad<T>(K.x, K.y, tag);
+            }
+        }
+        else if (comp == 4) {
+            if (R->SE != NULL) {
+                insertInTree(R->SE, K, tag);
+            }
+            else {
+                R->SE = new NodoQuad<T>(K.x, K.y, tag);
+            }
+        }
+    }
+
+    void insert(Punto K, T tag) {
+        if (root == NULL) {
+            root = new NodoQuad<T>(K.x, K.y, tag);
+            cout << "Árbol estaba vacío, insertando root" << endl;
+            return;
+        }
+
+        insertInTree(root, K, tag);
+    }
+
+    NodoQuad<T>* deleteNode(NodoQuad<T>* R, Punto K) {
+        if (R == NULL) return NULL;
+
+        // Si encontramos el nodo a eliminar
+        if (R->punto.x == K.x && R->punto.y == K.y) {
+            delete R;
+            return NULL;
+        }
+
+        int comp = compare(R->punto, K);
+
+        if (comp == 1) {
+            R->NW = deleteNode(R->NW, K);
+        }
+        else if (comp == 2) {
+            R->NE = deleteNode(R->NE, K);
+        }
+        else if (comp == 3) {
+            R->SW = deleteNode(R->SW, K);
+        }
+        else if (comp == 4) {
+            R->SE = deleteNode(R->SE, K);
+        }
+
+        return R;
+    }
+
+    void remove(Punto K) {
+        if (root == NULL) {
+            cout << "El árbol está vacío, no se puede eliminar." << endl;
+            return;
+        }
+
+        root = deleteNode(root, K);
+    }
+
+    void getStruct() {
+        if (!root) {
+            cout << "Árbol vacío." << endl;
+            return;
+        }
+
+        cout << "Raíz: " << root->dato << endl;
+        if (root->NW) cout << "Raíz NW: " << root->NW->dato << endl;
+        if (root->NE) cout << "Raíz NE: " << root->NE->dato << endl;
+        if (root->SW) cout << "Raíz SW: " << root->SW->dato << endl;
+        if (root->SE) cout << "Raíz SE: " << root->SE->dato << endl;
+    }
+
+
+
+    void display() {
+        if (root == NULL) {
+            cout << "Árbol vacío." << endl;
+        } else {
+            getStruct(root);
+        }
+    }
+
+    void draw(sf::RenderWindow& window, NodoQuad<T>* node, sf::VertexArray& lines) {
+        if (node == NULL) return;
+
+        sf::CircleShape point(5);
+        point.setFillColor(sf::Color::Red);
+        point.setPosition(node->punto.x, node->punto.y);
+        window.draw(point);
+
+        draw(window, node->NW, lines);
+        draw(window, node->NE, lines);
+        draw(window, node->SW, lines);
+        draw(window, node->SE, lines);
+    }
+};
