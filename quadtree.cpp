72707f7e57aed2ce191b5e7c5d01edd01a23c7f1
MirellaRP <68485108+MirellaRP@users.noreply.github.com>
2024-12-02 03:55:29 -0500

    Update quadtree.cpp

--- quadtree.cpp
+++ quadtree.cpp
@@ -36,73 +36,96 @@ public:
 
     int compare(Punto R, Punto K) {
         // Si retorna 1 es NW, si retorna 2 es NE, si es 3 SW, si es 4 es SE.
-        if (R.x > K.x && R.y > K.y) {
+        if (R.x > K.x && R.y >= K.y) {
             return 1;
         }
-        else if (R.x < K.x && R.y > K.y) {
+        else if (R.x <= K.x && R.y >= K.y) {
             return 2;
         }
         else if (R.x > K.x && R.y < K.y) {
             return 3;
         }
-        else if (R.x < K.x && R.y < K.y) {
+        else if (R.x <= K.x && R.y < K.y) {
             return 4;
         }
         //si es 0 está fuera del rango
         return 0;
     }
 
-    void insertInTree(NodoQuad<T>* R, Punto K, T tag) {
-        int comp = compare(R->punto, K);
 
-        if (comp == 1) {
-            if (R->NW != NULL) {
-                insertInTree(R->NW, K, tag);
-            }
-            else {
-                R->NW = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-        else if (comp == 2) {
-            if (R->NE != NULL) {
-                insertInTree(R->NE, K, tag);
-            }
-            else {
-                R->NE = new NodoQuad<T>(K.x, K.y, tag);
-            }
-        }
-        else if (comp == 3) {
-            if (R->SW != NULL) {
-                insertInTree(R->SW, K, tag);
-            }
-            else {
-                R->SW = new NodoQuad<T>(K.x, K.y, tag);
+    void insertInTree(NodoQuad<T>* R, Punto K, T tag){
+
+        int comp = compare(R->punto,K); // Si retorna 1 es NW, si retorna 2 es NE, si es 3 SW, si es 4 es SE, si es 0 esta fuera del rango.
+
+
+        if(comp==1){
+            // Si es NW entramos al cuadrante actual y verificamos si el cuadrante NW apunta a algo.
+            //Si apunta a algo vamos a verificar ese cuadrante recursivamente
+            if(R->NW!=NULL){
+                insertInTree(R->NW,K,tag);
+            }else{
+                // Si no apunta a nada vamos a crear un nodo y realizamos la insercion
+                NodoQuad<T>* nodo = new NodoQuad<T>(K.x,K.y,tag);
+                R->NW=nodo;
             }
         }
-        else if (comp == 4) {
-            if (R->SE != NULL) {
-                insertInTree(R->SE, K, tag);
-            }
-            else {
-                R->SE = new NodoQuad<T>(K.x, K.y, tag);
+        else if(comp==2){
+            // Si es NE entramos al cuadrante actual y verificamos si el cuadrante NE apunta a algo.
+            //Si apunta a algo vamos a verificar ese cuadrante recursivamente
+            if(R->NE!=NULL){
+                insertInTree(R->NE,K,tag);
+            }else{
+                // Si no apunta a nada vamos a crear un nodo y realizamos la insercion
+                NodoQuad<T>* nodo = new NodoQuad<T>(K.x,K.y,tag);
+                R->NE=nodo;
             }
         }
+        else if(comp==3){
+            // Si es SW entramos al cuadrante actual y verificamos si el cuadrante SW apunta a algo.
+            //Si apunta a algo vamos a verificar ese cuadrante recursivamente
+            if(R->SW!=NULL){
+                insertInTree(R->SW,K,tag);
+            }else{
+                // Si no apunta a nada vamos a crear un nodo y realizamos la insercion
+                NodoQuad<T>* nodo = new NodoQuad<T>(K.x,K.y,tag);
+                R->SW=nodo;
+            }
+        }
+        else if(comp==4){
+            // Si es NE entramos al cuadrante actual y verificamos si el cuadrante NE apunta a algo.
+            //Si apunta a algo vamos a verificar ese cuadrante recursivamente
+            if(R->NE!=NULL){
+                insertInTree(R->NW,K,tag);
+            }else{
+                // Si no apunta a nada vamos a crear un nodo y realizamos la insercion
+                NodoQuad<T>* nodo = new NodoQuad<T>(K.x,K.y,tag);
+                R->SE=nodo;
+            }
+        }
+
     }
 
-    void insert(Punto K, T tag) {
-        if (root == NULL) {
+
+    void insert(Punto K, T tag){
+
+        if(root==NULL){
             root = new NodoQuad<T>(K.x, K.y, tag);
-            cout << "Árbol estaba vacío, insertando root" << endl;
+            cout<<"Arbol estaba vacio, insertando root"<<endl;
             return;
         }
 
-        insertInTree(root, K, tag);
+        insertInTree(root,K, tag);
+
     }
+
+
+
+
 
 NodoQuad<T>* deleteNode(NodoQuad<T>* R, Punto K) {
     if (R == NULL) return NULL;
 
-    // Si encontramos el nodo a eliminar
+    // Si encontramos el nodo a eliminar se realiza esta parte
     if (R->punto.x == K.x && R->punto.y == K.y) {
         // Si el nodo tiene hijos, uno de los hijos subirá como padre
         if (R->NW != NULL) {
@@ -122,6 +145,8 @@ NodoQuad<T>* deleteNode(NodoQuad<T>* R, Punto K) {
         delete R;
         return NULL;
     }
+
+    // Si no se encuentra el nodo a eliminar aun, se compara y se busca el cuadrante correspondiente.
 
     int comp = compare(R->punto, K);
 
